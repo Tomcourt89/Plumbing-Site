7cf97d85b961c18f9826d75cb7b5f1cb922e25ba
Tom Court <Tomcourt1989@bitbucket.org>
2022-03-28 20:13:41 +0100

    js file created for and open/close function to bring up a newly created popup on all pages. Background added to all pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Script to open the popup
+
+function openQuote() {
+    document.getElementById("quotepop").style.display = "block";
+}
+
+function closeQuote() {
+    document.getElementById("quotepop").style.display = "none";
+}
